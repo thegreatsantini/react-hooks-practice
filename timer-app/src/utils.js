5f8f1bda9c8309e3d207821df66260daa237981e
thegreatsantini <lss13@my.fsu.edu>
2019-01-18 18:35:46 -0800

    modified milliseconds functionformat

--- timer-app/src/utils.js
+++ timer-app/src/utils.js
@@ -1,12 +1,11 @@
 export const millisecondsToHuman = ms => {
   const seconds = Math.floor((ms / 1000) % 60);
   const minutes = Math.floor((ms / 1000 / 60) % 60);
-  const hours = Math.floor(ms / 1000 / 60 / 60);
 
   const humanized = [
-    pad(hours.toString(), 2),
     pad(minutes.toString(), 2),
-    pad(seconds.toString(), 2)
+    pad(seconds.toString(), 2),
+    pad(ms.toString(), 2)
   ].join(":");
 
   return humanized;
