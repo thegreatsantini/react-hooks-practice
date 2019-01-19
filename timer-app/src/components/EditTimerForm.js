98beed451b222ab78356cd1f21d8ce0e17ec96a3
thegreatsantini <lss13@my.fsu.edu>
2019-01-18 18:40:11 -0800

    removed warnings

--- timer-app/src/components/EditTimerForm.js
+++ timer-app/src/components/EditTimerForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 import Card from "@material-ui/core/Card";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
--- timer-app/src/components/Timer.js
+++ timer-app/src/components/Timer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
