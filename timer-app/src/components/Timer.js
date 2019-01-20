bc09b652b0c57733752444c586c133b3538af31c
thegreatsantini <lss13@my.fsu.edu>
2019-01-20 10:38:51 -0800

    conditionally renders reset button

--- timer-app/src/components/Timer.js
+++ timer-app/src/components/Timer.js
@@ -3,9 +3,6 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
 import IncrementTime from "./IncrementTime";
 import { withStyles } from "@material-ui/core/styles";
@@ -18,10 +15,19 @@ const styles = theme => ({
     maxWidth: 360,
     backgroundColor: theme.palette.background.paper
   },
-  title: {
+  time: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
+  },
+  info: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start"
+  },
+  title: {},
+  description: {
+    fontSize: 18
   },
   button: {
     margin: theme.spacing.unit
@@ -40,35 +46,53 @@ function Timer(props) {
     update
   } = props;
   const [isRunning, runTimer] = useState(false);
+  const [display, displayReset] = useState(false);
+  
   return (
     <Card id={id} className={classes.root}>
       <CardContent>
-        <List component="nav">
+        <Typography
+          className={classes.time}
+          component="h2"
+          variant="h5"
+          gutterBottom
+        >
+          {!isRunning ? (
+            <div id="time">{time}</div>
+          ) : (
+            <div id="time">
+              <IncrementTime startingTime={time} />
+            </div>
+          )}
+        </Typography>
+        <div className={classes.info}>
           <Typography
             className={classes.title}
             component="h2"
             variant="h5"
             gutterBottom
           >
-            {!isRunning ? (
-              time
-            ) : (
-              <div id="time">
-                <IncrementTime startingTime={time} />
-              </div>
-            )}
+            {title}
           </Typography>
-          <ListItem button>
-            <ListItemText primary={title} secondary={description} />
-          </ListItem>
-        </List>
+          <Typography
+            className={classes.description}
+            component="h2"
+            variant="h5"
+            gutterBottom
+          >
+            {description}
+          </Typography>
+        </div>
         <Divider />
       </CardContent>
       <CardActions>
         {!isRunning ? (
           <Button
             color="primary"
-            onClick={() => runTimer(true)}
+            onClick={() => {
+              displayReset(true);
+              runTimer(true);
+            }}
             className={classes.button}
             size="small"
           >
@@ -79,14 +103,27 @@ function Timer(props) {
             color="inherit"
             className={classes.button}
             size="small"
-            onClick={function(e) {
-              const item = document.getElementById("time");
-              update(index, item.textContent);
+            onClick={function() {
+              const currentTime = document.getElementById("time");
+              update(index, currentTime.textContent);
               runTimer(false);
-              // console.log('clicked', )
             }}
           >
             Stop
+          </Button>
+        )}
+        {display && (
+          <Button
+            color="secondary"
+            className={classes.button}
+            size="small"
+            onClick={() => {
+              runTimer(false)
+              update(index, '00:00:00')
+              displayReset(false)
+            }}
+          >
+            Reset
           </Button>
         )}
         <Button
