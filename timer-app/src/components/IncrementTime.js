6b6bfca113d6a8d2a4491eea481db466e384d567
thegreatsantini <lss13@my.fsu.edu>
2019-01-21 20:01:09 -0800

    grows time when limit is reached

--- timer-app/src/components/IncrementTime.js
+++ timer-app/src/components/IncrementTime.js
@@ -1,27 +1,26 @@
-import { useState, useEffect } from "react";
-import { millisecondsToHuman } from "../utils";
+import React, { useState, useEffect } from "react";
+import { millisecondsToHuman, toMS } from "../utils";
+import "./growAnimation.css";
 
-const toMS = time => {
-  if (time === 0 ) return 0;
-  const splitTime = time.split(":");
-  const hours = parseInt(splitTime[0]) * 3600 * 1000;
-  const minutes = parseInt(splitTime[1]) * 1000;
-  return hours + minutes + parseInt(splitTime[2]);
-};
+
 
 export default function IncrementTimer(props) {
   const { startingTime } = props;
   const [counter, setCounter] = useState(toMS(startingTime));
-
+  if (counter === 1 ) {console.log(props)}
+  
   useEffect(() => {
     const interval = setInterval(() => {
       setCounter(counter => counter + 1);
     }, 1);
-
+    
     return () => {
       clearInterval(interval);
     };
   }, []);
-
-  return millisecondsToHuman(counter);
+  if (Math.round(counter) > 1000) {
+    return <div className="element">{millisecondsToHuman(counter)}</div>;
+  } else {
+    return <div>{millisecondsToHuman(counter)}</div>;
+  }
 }
