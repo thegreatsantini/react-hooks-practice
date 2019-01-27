a5b25c0932efb31351185318bc4262c475f32942
thegreatsantini <lss13@my.fsu.edu>
2019-01-26 18:59:42 -0800

    more stuff

--- giphy/src/App.js
+++ giphy/src/App.js
@@ -1,29 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import SearchInput from './components/SearchInput'
-import GifCard from './components/GifCard'
-import './App.css';
+import React, { useState, Suspense, lazy } from "react";
+import SearchInput from "./components/SearchInput";
+// import GifCard from "./components/GifCard";
+import "./App.css";
+import { Button } from "@material-ui/core";
 
-function query(search) {
-  const query = fetch(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.REACT_APP_API_KEY}`)
-  query.then(data => data.json()).then(res => console.log(res))
-  return search
+const Placholder = lazy(() => import("./components/Placeholder"));
+const GifCard = lazy(() => import("./components/GifCard"));
+
+async function query(search) {
+  return await fetch(
+    `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${
+      process.env.REACT_APP_API_KEY
+    }`
+  )
+    .then(data => data.json())
+    .then(res => res.data.map(gif => gif.id));
 }
 function App(props) {
-  const [search, setSearch] = useState('')
-    return (
-      <div className="App">
-        <div className="App-header">
-          <SearchInput
-          handleChange={ (value) => {
-            setSearch(value)
-          } } />
-          <p>
-            {query(search)}
-          </p>
-          <GifCard />
-        </div>
+  const [search, setSearch] = useState([]);
+  return (
+    <div className="App">
+      <div className="App-header">
+        <SearchInput
+          handleChange={value => {
+            query(value).then(res => {
+              setSearch(res);
+            });
+          }}
+        />
+        <Suspense
+          fallback={
+            <p style={{color: 'black'}}>LOADING........</p>
+          }
+        >
+          <GifCard ids={search} />
+        </Suspense>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default App;
